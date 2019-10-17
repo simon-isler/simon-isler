b8f3022922b3979e99482db2985dbe7b852621b5
simon <simon-isler@gmx.ch>
2019-10-17 08:25:55 +0200

    Add 404 page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Contact from "./views/Contact";
+import Contact from './views/Contact';
+import PageNotFound from './views/PageNotFound';
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
             path: '/contact',
             name: 'contact',
             component: Contact,
+        },
+        {
+            path: '*',
+            name: 'pageNotFound',
+            component: PageNotFound,
         }
     ]
 })
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 class="title-xl">Oops!</h1>
+        <h2 class="subtitle">We couldn't find this page.</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PageNotFound"
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/styles.scss';
+
+    div {
+        display: grid;
+        justify-items: center;
+
+        .title-xl {
+            align-self: end;
+            font-size: 8rem;
+            margin-bottom: 0;
+        }
+
+        .subtitle {
+            align-self: start;
+            color: $gray;
+        }
+    }
+</style>
