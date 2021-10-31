d467de9d35879f95b40eb48b13bac7f618689da5
simon <simon-isler@gmx.ch>
2021-10-31 11:31:12 +0100

    Migrate to a different mail service

--- server/api/config/mailer.js
+++ server/api/config/mailer.js
@@ -2,15 +2,10 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: 465,
-  secure: true,
+  service: process.env.MAIL_SERVICE,
   auth: {
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
