7edee866a790122d907db4f9da0c6847d0a5483a
simon <simon-isler@gmx.ch>
2022-07-07 17:01:11 +0200

    Setup new project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="portfolio flex justify-center items-center h-screen bg-light-blue shadow-lg">
+    <div class="bg-light-gray h-5/6 w-5/6 rounded-xl p-12 ">
+      <div class="flex gap-24 pt-44">
+        <div class="w-1/12">
+          <p>socials</p>
+        </div>
+        <div class="flex w-3/4">
+          <div>
+            <h1 class="text-4xl font-semibold pb-3">Hi, I'm Simon</h1>
+            <p class="text-xl font-normal text-gray-500">Software Developer</p>
+            <div class="pt-5 w-full">
+              <p>
+                I'm a Software Developer based in Zurich. I'm currently studying at the University of Applied Sciences in
+                Winterthur.
+                Next to my studies, I'm working part-time
+                <a href="https://renuo.ch" class="text-brown underline underline-offset-2 font-medium">@ Renuo</a>.
+              </p>
+
+              <a href="mailto:mail@simonisler.ch"></a>
+            </div>
+          </div>
+          <div class="w-1/2">
+            <img src="./assets/portrait.png" alt="Portrait" class="h-3/4 rounded-full">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
+
+.portfolio {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
